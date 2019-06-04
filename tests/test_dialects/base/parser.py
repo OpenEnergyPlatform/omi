83aa935a5c8c748dcf79e2c5868ed94c108480ad
MGlauer <martinglauer89@gmail.com>
2019-06-04 20:03:11 +0200

    Rename assertCompileableEqual to assert_compileable_equal

--- tests/test_dialects/base/parser.py
+++ tests/test_dialects/base/parser.py
@@ -7,21 +7,21 @@ from metadata_tool.structure import Compilable
 def _test_generic_parsing(parser, inp, res, **kwargs):
     if parser:
         result = parser.parse_from_file(inp)
-        assertCompileableEqual(result, res, **kwargs)
+        assert_compileable_equal(result, res, **kwargs)
 
 
-def assertCompileableEqual(first, second, nulls=frozenset(["none"])):
+def assert_compileable_equal(first, second, nulls=frozenset(["none"])):
     print(first, second)
     for key in (set(first.__dict__.keys()).union(set(second.__dict__.keys()))):
         l = getattr(first, key)
         r = getattr(second, key)
         if isinstance(l, Compilable):
-            assertCompileableEqual(l, r, nulls=nulls)
+            assert_compileable_equal(l, r, nulls=nulls)
         elif isinstance(l, list) and isinstance(r, list):
             assert len(l) == len(r), "Lists do not match ({} != {})".format(l, r)
-            for l0, r0 in zip(l, r):
+            for l0, r0 in zip(sorted(l), sorted(r)):
                 if isinstance(l0, Compilable):
-                    assertCompileableEqual(l0, r0, nulls=nulls)
+                    assert_compileable_equal(l0, r0, nulls=nulls)
                 else:
                     assert l0 == r0, "{} != {}".format(l0, r0)
         else:
