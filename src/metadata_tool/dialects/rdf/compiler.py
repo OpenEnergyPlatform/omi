e034f4c93b7b76f40118fc3a6fc311c9afbf1616
MGlauer <martinglauer89@gmail.com>
2019-06-05 17:20:06 +0200

    Design compiler more consistently
    a compiler unit does not add it to a parent node, but just returns the compiled unit

--- src/metadata_tool/dialects/rdf/compiler.py
+++ src/metadata_tool/dialects/rdf/compiler.py
@@ -18,7 +18,7 @@ from metadata_tool.dialects.rdf.namespace import OEO
 from metadata_tool.dialects.rdf.namespace import SCHEMA
 from metadata_tool.dialects.rdf.namespace import SKOS
 
-from typing import Dict
+from typing import Dict, Iterable
 
 LANG_DICT = {
     "eng": "http://publications.europa.eu/resource/authority/language/ENG",
@@ -32,14 +32,7 @@ LANG_DICT = {
 
 class RDFCompiler(Compiler):
     def visit_context(self, context: structure.Context, *args, **kwargs):
-        # context
-        graph = args[0]
-        parent = args[2]
-        graph.add((parent, FOAF.homepage, Literal(context.homepage)))
-        graph.add((parent, DCAT.contactpoint, Literal(context.contact)))
-        graph.add((parent, OEO.documentation, Literal(context.documentation)))
-        graph.add((parent, OEO.sourceCode, Literal(context.source_code)))
-        graph.add((parent, OEO.grantNo, Literal(context.grant_number)))
+        return Literal(context.homepage), Literal(context.contact), Literal(context.documentation), Literal(context.source_code), Literal(context.grant_number)
 
     def visit_person(self, person: structure.Person, *args, **kwargs):
         graph = args[0]
@@ -51,22 +44,19 @@ class RDFCompiler(Compiler):
 
     def visit_contributor(self, contributor: structure.Contributor, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         c = BNode()
-        graph.add((parent, DCTERMS.contributor, c))
         graph.add((c, DCTERMS.contributor, self.visit(contributor.contributor, *args, **kwargs)))
         graph.add((c, OEO.date, Literal(contributor.date, datatype=XSD.date)))
         graph.add((c, OEO.comment, Literal(contributor.comment)))
         graph.add((c, OEO.object, Literal(contributor.object)))
+        return c
 
     def visit_language(self, language: structure.Language, *args, **kwargs):
         return URIRef(LANG_DICT[language])
 
     def visit_spatial(self, spatial: structure.Spatial, *args, **kwargs):
         graph = args[0]
-        root = args[1]
         node = BNode()
-        graph.add((root, DCTERMS.spatial, node))
         graph.add((node, SKOS.prefLabel, Literal(spatial.extent)))
         graph.add((node, OEO.has_spatial_resolution, Literal(spatial.resolution)))
         graph.add((node, OEO.location, Literal(spatial.location)))
@@ -74,38 +64,30 @@ class RDFCompiler(Compiler):
 
     def visit_temporal(self, temporal: structure.Temporal, *args, **kwargs):
         graph = args[0]
-        parent = args[1]
         node = BNode()
-        graph.add((parent, DCTERMS.temporal, node))
         graph.add((node, RDF.type, DCTERMS.PeriodOfTime))
         graph.add((node, SCHEMA.startDate, Literal(temporal.ts_start)))
         graph.add((node, SCHEMA.endDate, Literal(temporal.ts_end)))
         graph.add((node, OEO.has_time_resolution, Literal(temporal.ts_resolution)))
         graph.add((node, OEO.referenceDate, Literal(temporal.reference_date)))
-        self.visit(temporal.ts_orientation, graph, node)
+        graph.add((node, OEO.has_orientation, self.visit(temporal.ts_orientation, graph)))
         return node
 
     def visit_timestamp_orientation(
         self, ts_orientation: structure.TimestampOrientation, *args, **kwargs
     ):
-        graph = args[0]
-        parent = args[1]
-
         if ts_orientation == structure.TimestampOrientation.left:
-            v = "left"
+            return OEO.left_orientation
         elif ts_orientation == structure.TimestampOrientation.middle:
-            v = "middle"
+            return OEO.middle_orientation
         elif ts_orientation == structure.TimestampOrientation.right:
-            v = "right"
+            return OEO.right_orientation
         else:
             raise Exception("Unknown timestamp orientation")
-        graph.add((parent, OEO.has_orientation, Literal(v)))
 
     def visit_source(self, source: structure.Source, *args, **kwargs):
         graph = args[0]
-        parent = args[1]
         node = BNode()
-        graph.add((parent, DCTERMS.source, node))
         graph.add((node, DCTERMS.title, Literal(source.title)))
         graph.add((node, DCTERMS.description, Literal(source.description)))
         graph.add((node, FOAF.page, Literal(source.path)))
@@ -119,24 +101,18 @@ class RDFCompiler(Compiler):
 
     def visit_license(self, lic: structure.License, *args, **kwargs):
         graph = args[0]
-        parent = args[1]
-
-        graph.add((parent, DCATDE.licenseAttributionByText, Literal(lic.attribution)))
-        graph.add((parent, DCATDE.licenseAttributionByText, Literal(lic.instruction)))
         if lic.name in LICENSE_DICT:
             li = URIRef(LICENSE_DICT[lic.name])
         else:
             li = BNode()
             graph.add((li, RDF.type, DCTERMS.LicenseDocument))
-            graph.add((parent, DCTERMS.title, Literal(lic.name)))
+            graph.add((li, DCTERMS.title, Literal(lic.name)))
             graph.add((li, DCTERMS.title, Literal(lic.title)))
             graph.add((li, OEO.path, Literal(lic.path)))
-        graph.add((parent, DCTERMS.license, li))
-        return li
+        return Literal(lic.attribution), Literal(lic.instruction), li
 
     def visit_resource(self, resource: structure.Resource, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         s = BNode()
         graph.add((s, RDF.type, DCAT.Distribution))
         graph.add((s, DCTERMS.title, Literal(resource.name)))
@@ -144,77 +120,77 @@ class RDFCompiler(Compiler):
         graph.add((s, OEO.has_format, Literal(resource.format)))  # dct:format ?
         graph.add((s, OEO.profile, Literal(resource.profile)))
         graph.add((s, OEO.encoding, Literal(resource.encoding)))
-        self.visit(resource.dialect, graph, args[1], s)
-        self.visit(resource.schema, graph, args[1], s)
-
-        graph.add((parent, OEO.has_resource, s))
+        graph.add((s, OEO.has_dialect, self.visit(resource.dialect, graph)))
+        fields, pks, fks = self.visit(resource.schema, graph)
+        for f in fields:
+            graph.add((s, OEO.field, f))
+        for pk in pks:
+            graph.add((s, OEO.primaryKey, pk))
+        for fk in fks:
+            graph.add((s, OEO.has_foreignKey, fk))
+        return s
 
     def visit_schema(self, schema: structure.Schema, *args, **kwargs):
-        graph = args[0]
-        parent = args[1]
-        field_dict = dict(
-            self.visit(field, *args, **kwargs) for field in schema.fields
-        )
-        for pk in schema.primary_key:
-            graph.add((parent, OEO.primaryKey, field_dict[pk]))
+        field_dict = dict()
+        fields = []
+        for field in schema.fields:
+            fnode = self.visit(field, *args, **kwargs)
+            fields.append(fnode)
+            field_dict[field.name] = fnode
+
+        pks = [field_dict[pk] for pk in schema.primary_key]
 
         if schema.fields:
             resource_dict = {schema.fields[0].resource.name: field_dict}
         else:
             resource_dict = {}
 
-        for fk in schema.foreign_keys:
-            self.visit(fk, *args, resouce_dict=resource_dict, **kwargs)
+        fks = [self.visit(fk, *args, resouce_dict=resource_dict, **kwargs)
+        for fk in schema.foreign_keys]
+
+
+        return fields, pks, fks
 
     def visit_dialect(self, dialect: structure.Dialect, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         node = BNode()
-        graph.add((parent, OEO.has_dialect, node))
         graph.add((node, OEO.delimiter, Literal(dialect.delimiter)))
         graph.add((node, OEO.decimalSeparator, Literal(dialect.decimal_separator)))
+        return node
 
     def visit_field(self, field: structure.Field, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         field_uri = BNode()
-        graph.add((parent, OEO.field, field_uri))
         graph.add((field_uri, RDF.type, OEO.DatabaseField))
         graph.add((field_uri, DCTERMS.title, Literal(field.name)))
         graph.add((field_uri, DCTERMS.description, Literal(field.description)))
         graph.add((field_uri, OEO.type, Literal(field.type)))
         graph.add((field_uri, OEO.unit, Literal(field.unit)))
-        return (field.name, field_uri)
+        return field_uri
 
     def visit_foreign_key(self, foreign_key: structure.ForeignKey, *args, resouce_dict=None, **kwargs):
         graph = args[0]
-        parent = args[2]
         fk_node = BNode()
-        graph.add((parent, OEO.has_foreignKey, fk_node))
-        fk_list = []
         for r in foreign_key.references:
-            self.visit(r, graph, args[1], fk_node, *args, resouce_dict=resouce_dict, **kwargs)
-
+            graph.add((fk_node, OEO.has_reference, self.visit(r, graph, *args, resouce_dict=resouce_dict, **kwargs)))
+        return fk_node
 
     def visit_reference(self, reference: structure.Reference, *args, resouce_dict=None, **kwargs):
         graph = args[0]
-        parent = args[2]
         r_node = BNode()
-        graph.add((parent, OEO.has_reference, r_node))
         graph.add((r_node, OEO.has_source, self._get_field(reference.source, resouce_dict)))
         graph.add((r_node, OEO.has_target, self._get_or_create_field(reference.target, resouce_dict, *args, **kwargs)))
+        return r_node
 
     def visit_review(self, review: structure.Review, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         node = BNode()
-        graph.add((parent, OEO.review, node))
         graph.add((node, OEO.path, URIRef(review.path)))
         graph.add((node, OEO.has_badge, Literal(review.badge)))
+        return node
 
     def visit_meta_comment(self, comment: structure.MetaComment, *args, **kwargs):
         graph = args[0]
-        parent = args[2]
         com = BNode()
         graph.add((com, OEO.metadata_info, Literal(comment.metadata_info)))
         graph.add((com, OEO.dates_info, Literal(comment.dates)))
@@ -223,7 +199,6 @@ class RDFCompiler(Compiler):
         graph.add((com, OEO.licenses_info, Literal(comment.licenses)))
         graph.add((com, OEO.review_info, Literal(comment.review)))
         graph.add((com, OEO.none_info, Literal(comment.none)))
-        graph.add((parent, OEO.comment, com))
         return com
 
     def visit_metadata(self, metadata: structure.OEPMetadata, *args, **kwargs):
@@ -258,17 +233,29 @@ class RDFCompiler(Compiler):
             )
         )
 
-        self.visit(metadata.context, g, datasetURI, datasetURI)
-        spatial = self.visit(metadata.spatial, g, datasetURI, datasetURI)
-        temporal = self.visit(metadata.temporal, g, datasetURI, datasetURI)
-        sources = [self.visit(s, g, datasetURI, datasetURI) for s in metadata.sources]
-        licenses = [self.visit(l, g, datasetURI, datasetURI) for l in metadata.license]
-        contributors = [
-            self.visit(c, g, datasetURI, datasetURI) for c in metadata.contributors
-        ]
-        resources = [
-            self.visit(r, g, datasetURI, datasetURI) for r in metadata.resources
-        ]
+        homepage, contact, documentation, source_code, grant_number = self.visit(metadata.context, g)
+
+        g.add((datasetURI, FOAF.homepage, homepage))
+        g.add((datasetURI, DCAT.contactpoint, contact))
+        g.add((datasetURI, OEO.documentation, documentation))
+        g.add((datasetURI, OEO.sourceCode, source_code))
+        g.add((datasetURI, OEO.grantNo, grant_number))
+
+        g.add((datasetURI, DCTERMS.spatial, self.visit(metadata.spatial, g)))
+        g.add((datasetURI, DCTERMS.temporal, self.visit(metadata.temporal, g)))
+        for s in metadata.sources:
+            g.add((datasetURI, DCTERMS.source, self.visit(s, g)))
+        for l in metadata.license:
+            attribution, instruction, license_node = self.visit(l, g)
+            g.add((datasetURI, DCATDE.licenseAttributionByText,attribution))
+            g.add((datasetURI, DCATDE.licenseAttributionByText,instruction))
+            g.add((datasetURI, DCTERMS.license, license_node))
+        for c in metadata.contributors:
+            g.add((datasetURI, DCTERMS.contributor, self.visit(c, g)))
+        for r in metadata.resources:
+            g.add((datasetURI, OEO.has_resource, self.visit(r, g)))
+
+        g.add((datasetURI, OEO.review, self.visit(metadata.review, g)))
 
         g.add(
             (
@@ -278,7 +265,7 @@ class RDFCompiler(Compiler):
             )
         )
 
-        comment_dict = self.visit(metadata.comment, g, datasetURI, datasetURI)
+        g.add((datasetURI, OEO.comment, self.visit(metadata.comment, g)))
 
         return g.serialize(format="turtle").decode("utf-8")
 
@@ -289,7 +276,7 @@ class RDFCompiler(Compiler):
                 resource_dict[field.resource.name] = {}
             return self._get_field(field, resource_dict)
         except FieldNotFoundError:
-            f = self.visit_field(field, *args, **kwargs)[1]
+            f = self.visit_field(field, *args, **kwargs)
             resource_dict[field.resource.name][field.name] = f
             return f
 
