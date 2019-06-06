ffd82bebd844f8a68a6cb388bc575b432b7993d7
MGlauer <martinglauer89@gmail.com>
2019-06-06 11:22:23 +0200

    Prevent circular references
    This can be implemented more properly by keeping a cache if objects hashes

--- tests/test_dialects/base/parser.py
+++ tests/test_dialects/base/parser.py
@@ -1,7 +1,7 @@
 import datetime
 import unittest
 
-from metadata_tool.structure import Compilable
+from metadata_tool.structure import Compilable, Field
 
 
 def _test_generic_parsing(parser, inp, res, **kwargs):
@@ -10,25 +10,35 @@ def _test_generic_parsing(parser, inp, res, **kwargs):
         assert_compileable_equal(result, res, **kwargs)
 
 
-def assert_compileable_equal(first, second, nulls=frozenset(["none"])):
-    print(first, second)
+def assert_compileable_equal(first, second, nulls=frozenset(["none"]), exclude=None):
+    exclude = exclude or []
     for key in (set(first.__dict__.keys()).union(set(second.__dict__.keys()))):
-        l = getattr(first, key)
-        r = getattr(second, key)
-        if isinstance(l, Compilable):
-            assert_compileable_equal(l, r, nulls=nulls)
-        elif isinstance(l, list) and isinstance(r, list):
-            assert len(l) == len(r), "Lists do not match ({} != {})".format(l, r)
-            for l0, r0 in zip(sorted(l), sorted(r)):
-                if isinstance(l0, Compilable):
-                    assert_compileable_equal(l0, r0, nulls=nulls)
-                else:
-                    assert l0 == r0, "{} != {}".format(l0, r0)
-        else:
-            if l in nulls and r is None:
-                return
+        if key not in exclude:
+            l = getattr(first, key)
+            r = getattr(second, key)
+            if isinstance(l, Compilable):
+                new_exclude = []
+                if isinstance(l, Field):
+                    new_exclude = ["resource"]
+                print(key, new_exclude, type(l), first)
+                assert_compileable_equal(l, r, nulls=nulls, exclude=new_exclude)
+            elif isinstance(l, list) and isinstance(r, list):
+                assert len(l) == len(r), "Lists do not match ({} != {})".format(l, r)
+                for l0, r0 in zip(sorted(l), sorted(r)):
+                    if isinstance(l0, Compilable):
+                        if isinstance(l0, Field):
+                            new_exclude = ["resource"]
+                        else:
+                            new_exclude = []
+                        print(key, new_exclude, type(l0), first)
+                        assert_compileable_equal(l0, r0, nulls=nulls, exclude=new_exclude)
+                    else:
+                        assert l0 == r0, "{} != {}".format(l0, r0)
+            else:
+                if l in nulls and r is None:
+                    return
 
-            assert l == r, "Keys {} do not match ({}:{} != {}:{})".format(key, type(l), l, type(r) ,r)
+                assert l == r, "Keys {} do not match ({}:{} != {}:{})".format(key, type(l), l, type(r) ,r)
 
 
 def parse_date(s):
