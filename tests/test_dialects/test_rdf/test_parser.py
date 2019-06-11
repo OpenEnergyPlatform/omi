f4ee4acf1b1b56bbf2eee46d15f788a9071602c1
MGlauer <martinglauer89@gmail.com>
2019-06-11 16:21:35 +0200

    Implement tests for rdf parser

---
+++ tests/test_dialects/test_rdf/test_parser.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from metadata_tool.dialects.rdf.parser import RDFParser
+from ..internal_structures import metadata_v_1_4
+from ..base.parser import _test_generic_parsing
+
+def test_parser_rdf():
+    parser = RDFParser()
+    _input_file = "tests/data/metadata_v14.ttl"
+    expected_result = metadata_v_1_4
+    _test_generic_parsing(parser, _input_file, expected_result)
+
+
