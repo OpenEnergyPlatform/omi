3a12a49597a27bbc0ed8ab26835281b62ab7ed5e
MGlauer <martinglauer89@gmail.com>
2019-06-12 15:13:11 +0200

    Adapt bumpversion to black style

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="omi",
-    version="version='0.0.1'",
+    version="0.0.1",
     license="AGPL-3.0",
     description="A library to process and translate open energy metadata.",
     long_description="%s\n%s"
