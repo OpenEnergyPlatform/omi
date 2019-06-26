2e3fb99bb29be615420fb13bd78c9e006eff92b4
MGlauer <martinglauer89@gmail.com>
2019-06-26 10:34:15 +0200

    Import implemted dialects directly

--- src/omi/dialects/__init__.py
+++ src/omi/dialects/__init__.py
@@ -1,7 +1,2 @@
-import pkgutil
-
-from omi.dialects.base.register import get_dialect
-
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    n = "omi.dialects." + module_name + ".dialect"
-    __import__(n)
+import omi.dialects.oep.dialect
+import omi.dialects.rdf.dialect
