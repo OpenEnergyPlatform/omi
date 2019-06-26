54f6a76bc0abdf3e514d5e72908eeb608fefed8b
MGlauer <martinglauer89@gmail.com>
2019-06-26 10:34:54 +0200

    Reorder imports

--- src/omi/dialects/oep/__init__.py
+++ src/omi/dialects/oep/__init__.py
@@ -1,1 +1,2 @@
-from omi.dialects.oep.dialect import OEP_V_1_4_Dialect, OEP_V_1_3_Dialect
+from omi.dialects.oep.dialect import OEP_V_1_3_Dialect
+from omi.dialects.oep.dialect import OEP_V_1_4_Dialect
