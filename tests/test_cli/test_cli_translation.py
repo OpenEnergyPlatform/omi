527b7675a9ebb0564d8bbe90b50e370443f079dc
MGlauer <martinglauer89@gmail.com>
2019-07-18 12:33:56 +0200

    Adapt tests for python3.5
    Python<3.6 does not preserve the order oof keys in dictionaries. Thus,
    we parse the json dicts in the tests.

--- tests/test_cli/test_cli_translation.py
+++ tests/test_cli/test_cli_translation.py
@@ -1,6 +1,7 @@
 import click
 from click.testing import CliRunner
 from omi.cli import translate
+import json
 
 def test_cli_translation():
     @click.command()
@@ -11,6 +12,6 @@ def test_cli_translation():
     runner = CliRunner()
     result = runner.invoke(translate, ["-f", "oep-v1.3", "-t", "oep-v1.4", "tests/data/metadata_v13.json"])
     with open("tests/data/metadata_v13_converted.json") as expected_file:
-        expected = expected_file.read()
+        expected = json.loads(expected_file.read())
         assert result.exit_code == 0
-        assert result.output == expected
+        assert json.loads(result.output) == expected
