125afb3dde926a5f22587546ab95d0e112f27179
MGlauer <martinglauer89@gmail.com>
2020-05-12 16:38:00 +0200

    Update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="omi",
-    version="0.0.4",
+    version="0.0.5",
     license="AGPL-3.0",
     description="A library to process and translate open energy metadata.",
     long_description="%s\n%s"
