75a83ea21f1deb2264c62c92b1882204cbff75c7
jh-RLI <jonas.huber@rl-institut.de>
2022-07-19 00:03:42 +0200

    add imports and base compiler class

--- src/omi/dialects/oep/conversion.py
+++ src/omi/dialects/oep/conversion.py
@@ -1,42 +1,70 @@
-import datetime
+from datetime import datetime
+import json
+import logging
+import pathlib
 
+from omi.dialects.base.dialect import Dialect
+from omi.dialects import get_dialect
 from omi import structure
-from omi.dialects.oep.compiler import JSONCompiler
-from omi.dialects.oep.parser import JSONParser_1_3
+from omi.oem_structures import oem_v15
 
+# NOTE: Maybe change this to abstract class and implement methods in concrete child classes
+class Converter:
+    """
+    Base Converter class for oemetadata version conversion.
+    Provides basic converstion functions and helpers.
+    All concrete conversion classes should inheret from this class.
 
-def metadata_conversion(old_sql, new_sql, user, user_email):
-    """ Conversion of an existing metadata file to a newer version
-
-    Parameters
-    ----------
-
-    old_sql: str
-        The path to the file containing the old sql file.
-    new_sql: str
-        The filename of the new sql file.
-    user: str
-        The name of the user for the 'contributions' section
-    user_email: str
-        The email address of the user.
-
-    Returns
-    -------
     """
 
-    parser = JSONParser_1_3()
-    metadata = parser.parse_from_file(old_sql)
+    def __init__(
+        self,
+        dielact_id: str = "oep-v1.5", #latest version
+        metadata: structure.OEPMetadata = None,
+    ) -> None:
+        self.dialect_id = dielact_id
+        self.metadata = metadata
 
-    metadata.contributors.append(
-        structure.Contribution(
-            title=user,
-            email=user_email,
-            date=datetime.now,
-            obj=None,
-            comment="Update metadata to v1.3 using metadata conversion tool",
+    def validate_str_version_format(self):
+        return NotImplementedError
+
+    def format_version_string(self, version_string: str = None) -> str:
+        if version_string is not None:
+            version_string = version_string.lower()
+            parts = version_string.split(sep="-")
+            self.dialect_id = parts[0] + "-v" + parts[1][:-2]
+        return self.dialect_id
+
+    def detect_oemetadata_dialect(self) -> Dialect:
+        try:
+            version: str = self.metadata["metaMetadata"]["metadataVersion"]
+            self.dialect_id = self.format_version_string(version_string=version)
+            logging.info(f"The dectected dialect is: {self.dialect_id}")
+        except Exception as e:
+            logging.warning(
+                {
+                    "exception": f"{e}",
+                    "message": f"Could not detect the dialect based on the Oemetadata json string. The key related to meta-metadata information might not be present in the input metadata json file. Fallback to the default dialect: '{self.dialect_id}'.",
+                }
+            )
+        return get_dialect(identifier=self.dialect_id)
+
+    #NOTE: Add omi version to user? 
+    def set_contribution(
+        self, metadata: oem_v15.OEPMetadata, user: str = "OMI", user_email: str = None
+    ) -> oem_v15.OEPMetadata:
+        contribution = oem_v15.Contribution(
+            contributor=oem_v15.Person(name=user, email=user_email),
+            date=datetime.now(),
+            obj="Metadata conversion",
+            comment="Update metadata to "
+            + self.dialect_id
+            + " using OMIs metadata conversion tool.",
         )
-    )
 
-    compiler = JSONCompiler()
-    with open(new_sql) as out_file:
-        out_file.write(compiler.visit(metadata))
+        metadata = metadata.contributions.append(contribution)
+
+        return metadata
+
+    def convert_oemetadata():
+        pass
