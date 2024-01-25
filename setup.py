62a85c8cc622e05e54fe0e3db6b1a14f371bfefb
Jonas Huber <jonas.huber@rl-institut.de>
2024-01-25 14:23:46 +0100

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="omi",
-    version="0.1.2",
+    version="0.2.0",
     license="AGPL-3.0",
     description="A library to process and translate open energy metadata.",
     long_description="%s\n%s"
