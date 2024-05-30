19f5826f699c913cb3c8d9c0ce7f8d9fa34802c0
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-05-30 09:32:47 +0200

    Read conversion chain recursively

--- src/omi/conversion.py
+++ src/omi/conversion.py
@@ -1,8 +1,12 @@
 """Conversion module for OMI to update metadata to different versions."""
 
+from copy import deepcopy
+
 from omi.base import extract_metadata_version
 
-METADATA_CONVERSION_CHAIN = {"OEP": ["OEP-1.5.2", "OEP-1.6.0"], "INSPIRE": []}
+
+class ConversionError(Exception):
+    """Raised when a conversion fails."""
 
 
 def convert_metadata(metadata: dict, target_version: str) -> dict:
@@ -22,11 +26,53 @@ def convert_metadata(metadata: dict, target_version: str) -> dict:
         Updated metadata
     """
     metadata_version = extract_metadata_version(metadata)
-    metadata_conversion_functions = {
-        ("OEP-1.5.2", "OEP-1.6.0"): __convert_oep_152_to_160,
-    }
-    metadata_conversion_function = metadata_conversion_functions[(metadata_version, target_version)]
-    return metadata_conversion_function(metadata)
+    conversion_chain = __get_conversion_chain(metadata_version, target_version)
+    converted_metadata = deepcopy(metadata)
+    for next_version in conversion_chain[1:]:
+        current_version = extract_metadata_version(converted_metadata)
+        converted_metadata = METADATA_CONVERSIONS[(current_version, next_version)](converted_metadata)
+    return converted_metadata
+
+
+def __get_conversion_chain(source_version: str, target_version: str) -> list[str]:
+    """
+    Try to find conversion chain from source version to target version.
+
+    Parameters
+    ----------
+    source_version: str
+        Starting version
+    target_version: str
+        Version goal
+
+    Raises
+    ------
+    ConversionError
+        if no conversion chain is found
+
+    Returns
+    -------
+    list[str]
+        List of conversion chain from source version to target version
+    """
+
+    def get_chain(current_version: str) -> list[str]:
+        for source, target in METADATA_CONVERSIONS:
+            if source != current_version:
+                continue
+            if target == target_version:
+                # Solution found! Return last conversion tuple
+                return [current_version, target_version]
+            child_chain = get_chain(target)
+            if child_chain is None:
+                continue
+            return [current_version, *child_chain]
+        return None
+
+    conversion_chain = get_chain(source_version)
+    if conversion_chain:
+        return conversion_chain
+    raise ConversionError(f"No conversion chain found from {source_version} to {target_version}.")
 
 
 def __convert_oep_152_to_160(metadata: dict) -> dict:
@@ -46,3 +92,8 @@ def __convert_oep_152_to_160(metadata: dict) -> dict:
     # No changes in metadata fields
     metadata["metaMetadata"]["metadataVersion"] = "OEP-1.6.0"
     return metadata
+
+
+METADATA_CONVERSIONS = {
+    ("OEP-1.5.2", "OEP-1.6.0"): __convert_oep_152_to_160,
+}
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -9,3 +9,38 @@ def test_conversion_from_oep_152_to_160():
     converted_metadata_152 = conversion.convert_metadata(metadata_schema_152, "OEP-1.6.0")
     assert base.extract_metadata_version(converted_metadata_152) == "OEP-1.6.0"
     validation.validate_metadata(converted_metadata_152)
+
+
+def test_conversion_chain():
+    """Test conversion chain with conversion tree structure."""
+
+    def a_b_conversion(md: dict) -> dict:
+        md["metaMetadata"]["metadataVersion"] = "b"
+        md["value"] = md["value"] * 2
+        return md
+
+    def a_c_conversion(md: dict) -> dict:
+        md["metaMetadata"]["metadataVersion"] = "c"
+        md["value"] = md["value"] * 3
+        return md
+
+    def c_d_conversion(md: dict) -> dict:
+        md["metaMetadata"]["metadataVersion"] = "d"
+        md["value"] = md["value"] * 4
+        return md
+
+    def c_e_conversion(md: dict) -> dict:
+        md["metaMetadata"]["metadataVersion"] = "e"
+        md["value"] = md["value"] * 5
+        return md
+
+    conversion.METADATA_CONVERSIONS[("a", "b")] = a_b_conversion
+    conversion.METADATA_CONVERSIONS[("a", "c")] = a_c_conversion
+    conversion.METADATA_CONVERSIONS[("c", "d")] = c_d_conversion
+    conversion.METADATA_CONVERSIONS[("c", "e")] = c_e_conversion
+
+    # Create dummy metadata in OEP format with version "a"
+    metadata = {"name": "a", "value": 10, "metaMetadata": {"metadataVersion": "a"}}
+    converted_metadata = conversion.convert_metadata(metadata, "e")
+    assert base.extract_metadata_version(converted_metadata) == "e"
+    assert converted_metadata["value"] == 10 * 3 * 5
