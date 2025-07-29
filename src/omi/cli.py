ac555f63e472d335f09d137365c0d52f0cc9f0e5
jh-RLI <jonas.huber@rl-institut.de>
2025-07-29 15:11:41 +0200

    #126: add cli function to create metadata json from yaml file

--- src/omi/cli.py
+++ src/omi/cli.py
@@ -15,7 +15,14 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 
+import json
+from pathlib import Path
+from typing import Union
+
 import click
+
+from omi.creation.creator import OEMetadataCreator
+from omi.creation.utils import load_yaml_metadata
 
 
 @click.group()
@@ -29,3 +36,27 @@ cli = click.CommandCollection(sources=[grp])
 def main() -> None:
     """Start click application."""
     cli()
+
+
+@click.command()
+@click.argument("yaml_file")
+@click.argument("output_file")
+def from_yaml(yaml_file: Union[str, Path], output_file: Union[str, Path]) -> None:
+    """
+    Generate OEMetadata from a YAML file and write it to an output file.
+
+    Parameters
+    ----------
+    yaml_file: Union[str, Path]
+        Path to the input YAML file containing dataset and resources.
+    output_file: Union[str, Path]
+        Path to the output file where the generated OEMetadata JSON will be saved.
+    """
+    version, dataset, resources = load_yaml_metadata(yaml_file)
+    generator = OEMetadataCreator()
+    metadata = generator.generate_metadata(dataset, resources)
+
+    with Path(output_file).open("w", encoding="utf-8") as f:
+        json.dump(metadata, f, indent=2)
+
+    print(f"OEMetadata written to {output_file}")  # noqa: T201
